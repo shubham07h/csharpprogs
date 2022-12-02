3677b6c6d2991c5c71dc414c8370bdf488bd18f2
shubham07h <shubham1609sharma@gmail.com>
2022-12-02 13:36:34 +0530

    commit

--- chsarpprogs/Program.cs
+++ chsarpprogs/Program.cs
@@ -1,35 +1,34 @@
-﻿int IS_PART_TIME = 1;
-
-int IS_FULL_TIME = 2;
-
-int EMP_RATE_PER_HOUR= 20; //Variables
-
-int empHrs = 0;
-
-int empWage = 0;
-
-Random random = new Random();
-
-//Computation
-
-int empCheck = random.Next(0, 3);
-
-if (empCheck == IS_PART_TIME)
+﻿//Variables int empHrs 0;
+class Program
 {
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    static void Main(string[] args)
+    {
 
-    empHrs = 4;
+        int empHrs = 0;
+        int empWage = 0;
 
+        Random random = new Random(); //Computation
+
+        int empCheck = random.Next(0, 3);
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
+                break;
+
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : " + empWage);
+    }
 }
-
-else if (empCheck == IS_FULL_TIME)
-{
-    empHrs = 8;
-
-}
-else
-{
-    empHrs = 0;
-}
-
-empWage = empHrs* EMP_RATE_PER_HOUR;
-Console.WriteLine("Emp Wage :"+ empWage);
