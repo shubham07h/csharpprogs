2f0757903d8daae684a3f80a0f6df4b8f1aaab3b
shubham07h <shubham1609sharma@gmail.com>
2022-12-02 12:38:54 +0530

    commit

--- chsarpprogs/Program.cs
+++ chsarpprogs/Program.cs
@@ -1,13 +1,25 @@
 ﻿
-int fulltime = 1;
-Random randobj = new Random();
-int checkpresent = randobj.Next(0, 2);
-Console.WriteLine("random number=" + checkpresent);
-if (checkpresent == fulltime)
+int IS_FULL_TIME = 1;
+
+int EMP_RATE_PER_HOUR = 20;
+
+//Variables
+
+int empHrs = 0;
+
+int empwage = 0;
+
+Random random = new Random(); //Computation
+
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
 {
-    Console.WriteLine("employee is present");
+    empHrs = 8;
+
 }
 else
-{
-    Console.WriteLine("employee is absent");
-}
+
+    empHrs = 0;
+
+empwage =empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage :" + empwage);
