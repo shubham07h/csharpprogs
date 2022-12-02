8e977c63cb41c469adb778d56ce5704eea7b5d4d
shubham07h <shubham1609sharma@gmail.com>
2022-12-02 12:13:20 +0530

    commited

--- chsarpprogs/Program.cs
+++ chsarpprogs/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("welcome");
+﻿
+int fulltime = 1;
+Random randobj = new Random();
+int checkpresent = randobj.Next(0, 2);
+Console.WriteLine("random number=" + checkpresent);
+if (checkpresent == fulltime)
+{
+    Console.WriteLine("employee is present");
+}
+else
+{
+    Console.WriteLine("employee is absent");
+}
