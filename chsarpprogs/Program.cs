71597abb222b7c747d00c67e354c0e23eff0c868
shubham07h <shubham1609sharma@gmail.com>
2022-12-04 18:57:03 +0530

    commit

--- chsarpprogs/Program.cs
+++ chsarpprogs/Program.cs
@@ -1,18 +1,16 @@
-﻿using System;
-
-namespace eucledian_distance
+﻿if (line1.startPoint == line2.startPoint && line1.endPoint == line2.endPoint)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double x1, x2, y1, y2;
-            x1 = 3;
-            x2 = 4;
-            y1 = 5;
-            y2 = 2;
-            var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-            Console.WriteLine($"The Eucledian distance between the two points is: {Math.Round(distance, 4)}");
-        }
-    }
+    Console.WriteLine("Line1 is equal to Line2");
+}
+
+//Greater
+if (line1.startPoint > line2.startPoint && line1.endPoint > line2.endPoint)
+{
+    Console.WriteLine("Line1 is greater than Line2");
+}
+
+//Less
+if (line1.startPoint < line2.startPoint && line1.endPoint < line2.endPoint)
+{
+    Console.WriteLine("Line1 is less than Line2");
 }
